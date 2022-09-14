e0cd9f193bfadb7327ea87200cab203b0e3802e1
KEATLLoevkilde <thom4a14@stud.kea.dk>
2022-09-14 12:33:00 +0200

    main methods: createSuperhero() displayAllSuperheroes() searchSuperhero()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static Database db = new Database();
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
 
-        Database db = new Database();
 
         while (true){
             int userChoice;
@@ -23,45 +25,59 @@ public class Main {
 
 
             if (userChoice == 1) {
-                System.out.print("Type in superheroName: ");
-                String superheroName = sc.nextLine();
-
-                System.out.print("Type in civilianName: ");
-                String civilianName = sc.nextLine();
-
-                System.out.print("Type in superPower: ");
-                String superPower = sc.nextLine();
-
-                System.out.print("Is the superhero human? (Yes/No): ");
-                String isHuman = sc.next();
-
-                System.out.print("Type in creationYear: ");
-                int creationYear = sc.nextInt();
-
-                System.out.print("Type in strengh value: ");
-                double strengh = sc.nextDouble();
-
-                db.addSuperhero(superheroName, civilianName, superPower, isHuman, creationYear, strengh);
-
-                System.out.println("\nSuperhero stored!");
-                System.out.println('\n');
+                createSuperhero();
 
             } else if (userChoice == 2) {
-                System.out.println("\nList of all superheroes:");
-                for (Superhero i: db.getAllSuperheroes()) {
-                    System.out.println(i);
-                    System.out.println('\n');
-                }
+                displayAllSuperheroes();
+
 
             } else if (userChoice == 3) {
-                System.out.println("Type in name, or part of name, of the superhero you wish to find: ");
-                System.out.println(db.searchSuperhero(sc.nextLine()));
-                System.out.println();
-                // TODO: 14-09-2022 Kan ikke oprette en Superhero som variabel så jeg kan håndtere null på en pæn måde.
+                searchSuperhero();
+
             } else {
                 System.exit(0);
             }
         }
 
+    }
+
+    public static void createSuperhero(){
+        System.out.print("Type in superheroName: ");
+        String superheroName = sc.nextLine();
+
+        System.out.print("Type in civilianName: ");
+        String civilianName = sc.nextLine();
+
+        System.out.print("Type in superPower: ");
+        String superPower = sc.nextLine();
+
+        System.out.print("Is the superhero human? (Yes/No): ");
+        String isHuman = sc.next();
+
+        System.out.print("Type in creationYear: ");
+        int creationYear = sc.nextInt();
+
+        System.out.print("Type in strengh value: ");
+        double strengh = sc.nextDouble();
+
+        db.addSuperhero(superheroName, civilianName, superPower, isHuman, creationYear, strengh);
+
+        System.out.println("\nSuperhero stored!");
+        System.out.println('\n');
+    }
+
+    public static void displayAllSuperheroes(){
+        System.out.println("\nList of all superheroes:");
+        for (Superhero i: db.getAllSuperheroes()) {
+            System.out.println(i);
+            System.out.println('\n');
+        }
+    }
+
+    public static void searchSuperhero(){
+        System.out.println("Type in name, or part of name, of the superhero you wish to find: ");
+        System.out.println(db.searchSuperhero(sc.nextLine()));
+        System.out.println();
+        // TODO: 14-09-2022 Kan ikke oprette en Superhero som variabel så jeg kan håndtere null på en pæn måde.
     }
 }
